31c38f77ed3e2ae2b35c879bec0a8443657b878a
MdNazmul9 <nazmul.cse.pust@gmail.com>
2022-01-02 00:17:25 +0600

    Layout

--- 6.label.py
+++ 6.label.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QDialog, QLabel
+from PyQt5.QtWidgets import QApplication, QDialog, QLabel, QVBoxLayout
 import sys
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtCore import QRect
@@ -8,10 +8,11 @@ class Window(QDialog):
         super().__init__()
         
         self.title = "Label" 
+     
         self.left = 200
-        self.top = 400
-        self.weidth = 600
-        self.hight = 400
+        self.top = 100
+        self.width = 600
+        self.height = 300
         self.iconName = "assets/home.png"
         
         self.InitWindow()
@@ -19,7 +20,19 @@ class Window(QDialog):
     def InitWindow(self):
         self.setWindowTitle(self.title)
         self.setWindowIcon(QtGui.QIcon(self.iconName))
-        self.setGeometry(self.left, self.top, self.weidth, self.hight)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        
+        vbox = QVBoxLayout()
+        label = QLabel("This is first label")
+        vbox.addWidget(label)
+        
+        label2 = QLabel("This is second label")
+        
+        label2.setFont(QtGui.QFont("Sanserif", 20))
+        label2.setStyleSheet('color:red')
+        vbox.addWidget(label2)
+        
+        self.setLayout(vbox)
         
         self.show()
         
@@ -28,7 +41,7 @@ if __name__=="__main__":
     window = Window()
     sys.exit(App.exec())
     
-    
+
         
         
     
