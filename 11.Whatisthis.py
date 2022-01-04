96c0a1ad92c18453647e0d126ba81189ed9e016a
MdNazmul9 <nazmul.cse.pust@gmail.com>
2022-01-04 21:24:23 +0600

    whatsThis

--- 11.Whatisthis.py
+++ 11.Whatisthis.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget,QDialog, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QHBoxLayout, QPushButton, QWidget,QDialog, QLabel, QVBoxLayout
 import sys
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtCore import QRect
@@ -22,8 +22,20 @@ class Window(QWidget):
         self.setWindowTitle(self.title)
         self.setWindowIcon(QtGui.QIcon(self.iconName))
         self.setGeometry(self.left, self.top, self.width, self.height)
-    
+
+        hbox = QHBoxLayout()
+        label = QLabel("Focus and Press Shift + F1")
+        hbox.addWidget(label)
+        
+        btn = QPushButton("Click me", self)
+        btn.setWhatsThis("Display at when press shift+f1 and focus it")
+        hbox.addWidget(btn)
+        self.setLayout(hbox)
+        
         self.show()
+        
+        
+    
         
 if __name__=="__main__":
     App = QApplication(sys.argv)
