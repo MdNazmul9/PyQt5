1b54dc3f00646cc7512a0de905c30497310c6b54
MdNazmul9 <nazmul.cse.pust@gmail.com>
2022-01-04 23:03:20 +0600

    resize frameless window with cursor midle

--- 15.FramelessWindow.py
+++ 15.FramelessWindow.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QPushButton, QRadioButton, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QButtonGroup, QGroupBox
+from PyQt5.QtWidgets import QApplication, QPushButton, QRadioButton, QSizeGrip, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QButtonGroup, QGroupBox
 import sys
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtCore import QRect
@@ -24,11 +24,16 @@ class Window(QWidget):
         self.setGeometry(self.left, self.top, self.width, self.height)
         
         flags = QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint|QtCore.Qt.WindowStaysOnTopHint)
-        
         self.setWindowFlags(flags)
+        
+        vbox = QVBoxLayout()
+        sizegrip = QSizeGrip(self)
+        vbox.addWidget(sizegrip)
+        self.setLayout(vbox)
+        
+        
         self.show()
         
-    
 if __name__=="__main__":
     App = QApplication(sys.argv)
     window = Window()
